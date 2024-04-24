ff23cdb665e4662c8f0702df53856a8e313b7679
Vitor Ciscoto <89476671+vitorc7@users.noreply.github.com>
2024-04-24 19:06:25 -0300

    add task3 distinct list

--- myinterview/src/main/java/com/example/TASK3.java
+++ myinterview/src/main/java/com/example/TASK3.java
@@ -1,14 +1,36 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
-/**
- * Write a list and add an aleatory number of Strings. In the end, print out how
- * many distinct itens exists on the list.
- *
- */
 public class TASK3 {
 
-  
+  public static void main(String[] args) {
+    // Criando a lista
+    List<String> lista = new ArrayList<>();
+
+    // Adicionando strings aleatórias à lista
+    lista.add("Maçã");
+    lista.add("Banana");
+    lista.add("Laranja");
+    lista.add("Maçã");
+    lista.add("Abacaxi");
+    lista.add("Pêra");
+    lista.add("Melão");
+    lista.add("Banana");
+
+    // Exibindo a lista
+    System.out.println("\nLista de strings:");
+    // Utilizando Iterator para percorrer a lista
+    Iterator<String> iterator = lista.iterator();
+    while (iterator.hasNext()) {
+      String str = iterator.next();
+      System.out.println(str);
+    }
+
+    // Criando um conjunto para armazenar os itens únicos
+    Set<String> itensUnicos = new HashSet<>(lista);
+
+    // Exibindo o número de itens distintos
+    System.out.println("\nNúmero de itens distintos na lista: " + itensUnicos.size());
+  }
 }
